3a8afb56f59365b7000621fbbd600f716c56a11d
Kamilla Khisamieva <khisamieva0409@gmail.com>
2024-08-11 22:57:14 +0300

    remove github access token

--- src/api/constant.ts
+++ src/api/constant.ts
@@ -1,5 +1,5 @@
 export const GITHUB_API_URL = import.meta.env.VITE_GITHUB_API_URL;
-export const GITHUB_ACCESS_TOKEN = import.meta.env.VITE_GITHUB_ACCESS_TOKEN;
+// export const GITHUB_ACCESS_TOKEN = import.meta.env.VITE_GITHUB_ACCESS_TOKEN;
 
 export const pagination = {
   limit: 30,
