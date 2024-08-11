ac23a68262a3be80ad39c0881734b77283a537ab
Kamilla Khisamieva <khisamieva0409@gmail.com>
2024-08-11 23:00:25 +0300

    add margin bottom

--- src/component/Layout/Layout.tsx
+++ src/component/Layout/Layout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 const Layout = () => {
   return (
     <div className="container">
-      <div className="grid gap-12 mt-8">
+      <div className="grid gap-12 my-8">
         <Outlet />
       </div>
     </div>
